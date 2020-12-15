332eb065c7e6cbde9cd353c0207002888f1fd883
Parker Stanley <parker.stanley@somoglobal.com>
2020-12-15 12:59:48 -0600

    cleaned up sql file

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -23,18 +23,6 @@ WHERE Location = "St. Louis City";
 
 --Part 3
 
-write a query to return a list of the names
-and descriptions of all skills that are attached to jobs
-in alphabetical order.
-If a skill does not have a job listed, it should not be included in the results of this query.
-
-SELECT Name, Description
-FROM techjobs.Jobs
-INNER JOIN Skills ON Jobs.Id = JobSkills.Job.Id
-WHERE Job is not null
-ORDER BY Name DESC;
-
--- this is better
 SELECT Name, Description
 FROM Skills
 INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillId
